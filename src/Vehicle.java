b4d1db03b6681ef0ea85b1a105f661bc7c7fe099
lloydkev000 <lloydkevin631@gmail.com>
2024-09-12 13:54:25 -0400

    Finished

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Vehicle {
+public class Vehicle {
+
+    public String make;
+    public String model;
+
+
+    public Vehicle(String make, String model){
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
